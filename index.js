5fb8791e8e46993199fc8866e27a013b57edea21
piotrkoson <piotr@koson.pl>
2018-08-26 15:31:35 +0200

    Add custom event listener

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // @flow
+import TableHeader from './components/TableHeader/index';
+
+window.addEventListener('sortByName', e => console.log('object', e.detail));
+window.customElements.define('table-header', TableHeader);
