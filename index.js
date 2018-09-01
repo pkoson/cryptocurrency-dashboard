e3ba667b708be90ec958f8adaef6d5e06cf16aab
piotrkoson <piotr@koson.pl>
2018-09-01 18:44:23 +0200

    Define column-header web component

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // @flow
+import ColumnHeader from './components/ColumnHeader/index';
 import TableHeader from './components/TableHeader/index';
 
 window.addEventListener('sortByName', e => console.log('object', e.detail));
+window.customElements.define('column-header', ColumnHeader);
 window.customElements.define('table-header', TableHeader);
