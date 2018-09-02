dbeeda098f04c2412697863a5e4c730aea79bd47
piotrkoson <piotr@koson.pl>
2018-09-02 20:55:56 +0200

    Rename saga file

--- sagas/index.js
+++ sagas/index.js
@@ -3,10 +3,10 @@ import { takeEvery, all } from 'redux-saga/effects';
 import { GET_CURRENCIES } from '../actions/command';
 import { getCurrenciesListSaga } from './getCurrenciesList';
 
-function* watchgetCurrenciesList() {
+function* watchGetCurrenciesList() {
   yield takeEvery(GET_CURRENCIES, getCurrenciesListSaga);
 }
 
 export default function* rootSaga() {
-  yield all([watchgetCurrenciesList()]);
+  yield all([watchGetCurrenciesList()]);
 }
