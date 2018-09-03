5b0895ca587f709108bef2f17a56bd4220a2de01
piotrkoson <piotr@koson.pl>
2018-09-03 21:30:06 +0200

    Add GET_MULTIPLE_PRICE action

--- actions/command.js
+++ actions/command.js
@@ -1,9 +1,16 @@
 // @flow
 export const GET_CURRENCIES = 'command@GET_CURRENCIES';
+export const GET_MULTIPLE_PRICE = 'command@GET_MULTIPLE_PRICE';
 
-export function getCurrenciesList(payload) {
+export function getCurrenciesList() {
   return {
-    type: GET_CURRENCIES,
-    payload
+    type: GET_CURRENCIES
+  };
+}
+
+export function getMultiplePrice(list: Array<string>) {
+  return {
+    type: GET_MULTIPLE_PRICE,
+    list
   };
 }
