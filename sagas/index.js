4a804805bd2f0bd7ac93d37437169bec698a57a6
piotrkoson <piotr@koson.pl>
2018-09-03 21:30:35 +0200

    import new sagas

--- sagas/index.js
+++ sagas/index.js
@@ -1,12 +1,16 @@
 // @flow
 import { takeEvery, all } from 'redux-saga/effects';
-import { GET_CURRENCIES } from '../actions/command';
-import { getCurrenciesListSaga } from './getCurrenciesList';
+import { GET_CURRENCIES, GET_MULTIPLE_PRICE } from '../actions/command';
+import getCurrenciesListSaga from './getCurrenciesList';
+import getMultiplePriceSaga from './getMultiplePrice';
 
 function* watchGetCurrenciesList() {
   yield takeEvery(GET_CURRENCIES, getCurrenciesListSaga);
 }
+function* watchGetMultiplePrice() {
+  yield takeEvery(GET_MULTIPLE_PRICE, getMultiplePriceSaga);
+}
 
-export default function* rootSaga() {
-  yield all([watchGetCurrenciesList()]);
+export default function* rootSaga(): Generator<*, *, *> {
+  yield all([watchGetCurrenciesList(), watchGetMultiplePrice()]);
 }
