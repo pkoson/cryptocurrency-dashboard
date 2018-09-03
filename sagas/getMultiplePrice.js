8aabda9f42c53df33af986179d8616d4dc77316c
piotrkoson <piotr@koson.pl>
2018-09-03 21:30:49 +0200

    Add new saga

---
+++ sagas/getMultiplePrice.js
@@ -0,0 +1,16 @@
+// @flow
+import { call, put } from 'redux-saga/effects';
+import { currenciesPrice } from '../actions/document';
+
+export default function* getMultiplePrice({ list }) {
+  try {
+    const data = yield call(
+      fetch,
+      `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${list}&tsyms=USD`
+    );
+    const json = yield data.json();
+    yield put(currenciesPrice(json));
+  } catch (error) {
+    // TODO: put error aciton
+  }
+}
