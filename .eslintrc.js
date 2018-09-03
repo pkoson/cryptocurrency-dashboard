1b6f3dd8f1acf63e86e51cb03e45104f792182ed
piotrkoson <piotr@koson.pl>
2018-09-03 21:29:29 +0200

    Add babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'plugin:flowtype/recommended'],
   env: {
     browser: true,
     jest: true
-  }
+  },
+  parser: 'babel-eslint',
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'class-methods-use-this': ['error', { exceptMethods: ['connectedCallback'] }]
+  },
+  plugins: ['flowtype']
 };
