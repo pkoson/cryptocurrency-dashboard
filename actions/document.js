5758b337126186df3632b292e546b5449c7ed167
piotrkoson <piotr@koson.pl>
2018-09-03 21:30:22 +0200

    Add CURRENCIES_PRICE action

--- actions/document.js
+++ actions/document.js
@@ -1,8 +1,15 @@
 export const CURRENCIES = 'document@CURRENCIES';
+export const CURRENCIES_PRICE = 'document@CURRENCIES_PRICE';
 
 export function currencies(payload) {
   return {
     type: CURRENCIES,
+    payload
+  };
+}
+export function currenciesPrice(payload) {
+  return {
+    type: CURRENCIES_PRICE,
     payload
   };
 }
