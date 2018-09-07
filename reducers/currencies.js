cabcc107daad702b223feacef2ca8e115930072e
piotrkoson <piotr@koson.pl>
2018-09-07 11:53:26 +0200

    Handle FILTER_BY action

--- reducers/currencies.js
+++ reducers/currencies.js
@@ -1,5 +1,6 @@
+import { actionChannel } from 'redux-saga/effects';
 import { CURRENCIES, CURRENCIES_PRICE } from '../actions/document';
-import { SORT_CURRENCIES_BY } from '../actions/command';
+import { SORT_CURRENCIES_BY, FILTER_BY } from '../actions/command';
 
 export default function currencies(state = {}, { payload, type }) {
   switch (type) {
@@ -20,7 +21,7 @@ export default function currencies(state = {}, { payload, type }) {
       const getData = data => data.CoinInfo[name] || data.financialData[name];
       return {
         ...state,
-        list: state.list.sort((a, b) => {
+        list: (state.displayList || state.list).sort((a, b) => {
           if (getData(a) < getData(b)) {
             return direction === 0 ? 1 : -1;
           }
@@ -31,7 +32,34 @@ export default function currencies(state = {}, { payload, type }) {
         })
       };
     }
+    case FILTER_BY: {
+      const { name, value } = payload;
+      return {
+        ...state,
+        filtredList: {
+          ...state.filtredList,
+          [name]: state.list.reduce((pV, cV) => {
+            if (cV.CoinInfo[name].toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+              pV.push(cV.CoinInfo.Name);
+            }
+            return pV;
+          }, [])
+        }
+      };
+    }
     default:
       return state;
   }
 }
+
+function getFiltredList({ filtredList, list = [] }) {
+  if (!filtredList) return list;
+  const keys = Object.keys(filtredList);
+  if (keys.length === 1) return list.filter(item => filtredList[keys[0]].indexOf(item.CoinInfo.Name) >= 0);
+  const commonList = filtredList.Name.filter(element => filtredList.FullName.indexOf(element) >= 0);
+  return list.filter(item => commonList.indexOf(item.CoinInfo.Name) >= 0);
+}
+
+export const selectors = {
+  getFiltredList
+};
