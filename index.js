d0991d641fe9b191369442a460642c0d98b53d30
piotrkoson <piotr@koson.pl>
2018-09-07 11:53:01 +0200

    Define elements for FilterPanel

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import 'babel-polyfill';
 import ColumnHeader from './components/ColumnHeader';
 import TableHeader from './components/TableHeader';
 import TableRows from './components/TableRows';
+import FilterPanel from './components/FilterPanel';
 
-window.addEventListener('sortByName', e => console.log('object', e.detail));
 window.customElements.define('column-header', ColumnHeader);
 window.customElements.define('table-header', TableHeader);
 window.customElements.define('table-rows', TableRows);
+window.customElements.define('filter-panel', FilterPanel);
