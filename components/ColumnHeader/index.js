a63e5bbf33f316a34e8cbf05247d1ca1d1692970
piotrkoson <piotr@koson.pl>
2018-09-07 11:51:25 +0200

    Fix sortAction dispatcher

--- components/ColumnHeader/index.js
+++ components/ColumnHeader/index.js
@@ -1,25 +1,26 @@
 // @flow
+import store from '../../store';
+import { sortCurrenciesBy } from '../../actions/command';
 import styles from './style.css';
 
 export default class ColumnHeader extends HTMLElement {
+  name: string;
+
+  direction: 0 | 1;
+
   constructor() {
     super();
     const wrapper = document.createElement('div');
     const text = this.getAttribute('text') || 'pass text attr!';
     this.name = this.getAttribute('name') || '';
-    this.direction = 0;
-
     wrapper.textContent = text;
     wrapper.addEventListener('click', () => this.handleClick());
-    wrapper.setAttribute('class', styles.className);
+    this.setAttribute('class', styles.className);
     this.appendChild(wrapper);
   }
 
   handleClick() {
-    const event = new CustomEvent(`sortBy${this.name}`, {
-      detail: { direction: this.direction }
-    });
-    window.dispatchEvent(event);
-    this.direction = this.direction === 1 ? 0 : 1;
+    this.direction = this.direction === 0 ? 1 : 0;
+    store.dispatch(sortCurrenciesBy({ name: this.name, direction: this.direction }));
   }
 }
