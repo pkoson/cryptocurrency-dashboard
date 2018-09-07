55c3862e43f73d366348c848868093f2ac681881
piotrkoson <piotr@koson.pl>
2018-09-07 11:51:00 +0200

    Add new command actions

--- actions/command.js
+++ actions/command.js
@@ -1,6 +1,8 @@
 // @flow
 export const GET_CURRENCIES = 'command@GET_CURRENCIES';
 export const GET_MULTIPLE_PRICE = 'command@GET_MULTIPLE_PRICE';
+export const SORT_CURRENCIES_BY = 'command@SORT_CURRENCIES_BY';
+export const FILTER_BY = 'command@FILTER_BY';
 
 export function getCurrenciesList() {
   return {
@@ -12,5 +14,17 @@ export function getMultiplePrice(list: Array<string>) {
   return {
     type: GET_MULTIPLE_PRICE,
     list
+  };
+}
+export function sortCurrenciesBy(payload: { name: string, direction: 0 | 1 }) {
+  return {
+    type: SORT_CURRENCIES_BY,
+    payload
+  };
+}
+export function filterByFullName(payload: { name: string, value: EventTarget }) {
+  return {
+    type: FILTER_BY,
+    payload
   };
 }
