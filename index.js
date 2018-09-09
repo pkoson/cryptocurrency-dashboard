b4bb7049b214a1628845500d02e19c5713983a4a
piotrkoson <piotr@koson.pl>
2018-09-09 13:50:22 +0200

    Import new table components

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ import ColumnHeader from './components/ColumnHeader';
 import TableHeader from './components/TableHeader';
 import TableRows from './components/TableRows';
 import FilterPanel from './components/FilterPanel';
+import FilterPanelSection from './components/FilterPanelSection';
+import TableWrapper from './components/TableWrapper';
 
 window.customElements.define('column-header', ColumnHeader);
 window.customElements.define('table-header', TableHeader);
 window.customElements.define('table-rows', TableRows);
 window.customElements.define('filter-panel', FilterPanel);
+window.customElements.define('filter-panel-section', FilterPanelSection);
+window.customElements.define('table-wrapper', TableWrapper);
